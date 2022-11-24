9e6138ace5a573e2c6901618ec7cb206c03b4af3
khlzemil <emilmg@code.edu.az>
2022-11-24 14:51:42 +0400

    N-tier architecture

---
+++ TestApp/DataAccess/Repositories/Concrete/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Core.Entities.Base;
+using DataAccess.Contexts;
+using DataAccess.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _dbSet = appDbContext.Set<T>();
+        }
+        public async Task<List<T>> GetAllAsync()
+        {
+             return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+             await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+    }
+}
---
+++ TestApp/Web/Services/Abstract/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Web.ViewModels.Category;
+
+namespace Web.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<CategoryIndexVM> GetAllAsync();
+        Task<CategoryUpdateVM> GetUpdateModelAsync(int id);
+        Task<bool> CreateAsync(CategoryCreateVM model);
+        Task UpdateAsync(CategoryUpdateVM model);
+
+        Task DeleteAsync(int id);
+
+    }
+}
